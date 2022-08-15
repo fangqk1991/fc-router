55aaf66a89d8aabb18f9604d76f20653bcef68c9
Fang <me@fangqk.com>
2022-08-15 10:30:11 +0800

    v0.4.1: remove RouterSdkOptions.onPluginsCreated

--- src/sdk/RouterSdkOptions.ts
+++ src/sdk/RouterSdkOptions.ts
@@ -2,13 +2,11 @@ import * as Koa from 'koa'
 import { WriteLogMiddlewareBuilder } from '@fangcha/logger/lib/koa'
 import { FangchaSession } from '@fangcha/backend-kit'
 import { RouterApp } from '../main'
-import { AppPluginProtocol } from '@fangcha/backend-kit/lib/basic'
 
 export interface RouterSdkOptions {
   backendPort: number
   routerApp: RouterApp
   handleAuth: (ctx: Koa.Context) => Promise<void>
-  onPluginsCreated?: (plugins: AppPluginProtocol[]) => void
   onRequestError?: (err: Error, ctx: Koa.Context) => void
   customWriteLogMiddlewareBuilder?: WriteLogMiddlewareBuilder
   Session?: typeof FangchaSession & any
